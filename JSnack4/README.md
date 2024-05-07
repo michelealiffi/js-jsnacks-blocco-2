e31b6f5966c1835751a5b8da7b5d0483467ad20a
michelealiffi <michelealiffi890@gmail.com>
2024-05-07 10:10:22 +0200

    First commit

---
+++ JSnack4/README.md
@@ -0,0 +1,3 @@
+-Creare due array che hanno un numero di elementi diversi
+-Aggiungere elementi casuali all’array che ha meno elementi
+fino a quando ne avrà tanti quanti l’altro
---
+++ JSnack4/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let array1 = ["Gino", "Pippo"];
+let array2 = ["Sorbillo", "Bastianich", "Munchen", "Salvatiello"];
+let addArray = '';
+
+do{
+    if(array1.length < array2.length) {
+        addArray = prompt('Inserire nuovo elemento');
+        array1.push(addArray);
+    } else if (array1.length > array2.length) {
+        addArray = prompt('Inserire nuovo elemento');
+        array2.push(addArray);
+    } else {
+        console.log('Le liste sono uguali');
+    }
+
+} while(array1.length < array2.length || array1.length > array2.length); 
+
+console.log('Sei fuori');
